cab121bc1de55b8c5060c9a4a4513aa25663632e
Brad Pizzimenti <brad@gennakersystems.com>
2016-04-27 07:21:18 -0700

    add linked in activity

---
+++ app/src/main/java/com/gennakersystems/linkedhub/linkedInQuery.java
@@ -0,0 +1,13 @@
+package com.gennakersystems.linkedhub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class linkedInQuery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linked_in_query);
+    }
+}
