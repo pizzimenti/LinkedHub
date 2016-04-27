7d63f2c32d06dec540b657c983bae2a283727cfd
Brad Pizzimenti <brad@gennakersystems.com>
2016-04-27 07:21:46 -0700

    add linked in activity

--- app/src/main/java/com/gennakersystems/linkedhub/MainActivity.java
+++ app/src/main/java/com/gennakersystems/linkedhub/MainActivity.java
@@ -1,13 +1,28 @@
 package com.gennakersystems.linkedhub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mAuthButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuthButton = (Button) findViewById(R.id.authButton);
+
+        mAuthButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, linkedInQuery.class);
+                startActivity(intent);
+            }
+        });
     }
 }
